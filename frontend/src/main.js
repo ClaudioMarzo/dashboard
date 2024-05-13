f16914c004db126a81b05b5901556765eeb92f9a
ClaudioMarzo <claudio.m.santarem.16@gmail.com>
2024-05-13 19:17:44 -0400

    refactor: ajuste inicial do projeto

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index_router'
 
-createApp(App).use(router).mount('#app')
+import 'animate.css'
+import '@/assets/css/tailwindcss.css'
+import '@/assets/css/fonts.css'
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
